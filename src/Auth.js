95651f2f6cf561d23a902072fbaed8313025a2f5
mrwadepro <mrwadepro@gmail.com>
2021-02-08 22:15:17 -0600

    url chjange

--- src/Auth.js
+++ src/Auth.js
@@ -1,45 +1,31 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import urlParams from "./util/urlParams";
 import axios from "axios";
 function Auth() {
-  var method = "POST";
+  const [state, setState] = useState({
+    scope: urlParams("scope"),
+    type: urlParams("response_type"),
+    mode: urlParams("response_mode"),
+    prompt: urlParams("prompt"),
+    nonce: urlParams("nonce"),
+    client_id: urlParams("client_id"),
+    loginHint: urlParams("login_hint"),
+    state: urlParams("state"),
+    id_token: "jwt_token",
+  });
 
   useEffect(() => {
-    // Update the document title using the browser API
-    var form = document.querySelector("#authForm");
-    if (form) {
-    }
+    const url = urlParams("redirect_uri");
 
-    const params = {
-      scope: urlParams("scope"),
-      type: urlParams("response_type"),
-      mode: urlParams("response_mode"),
-      prompt: urlParams("prompt"),
-      nonce: urlParams("nonce"),
-      client_id: urlParams("client_id"),
-      loginHint: urlParams("login_hint"),
-      state: urlParams("state"),
-      id_token: "jwt_token",
-    };
-
-    // form.action = urlParams("redirect_uri");
-    // form.method = method;
-    // for (var name in params) {
-    //   var node = document.createElement("input");
-    //   node.name = name;
-    //   node.type = "hidden";
-    //   node.value = params[name];
-    //   form.appendChild(node);
-    // }
-    // var output = document.querySelector("code");
-    // output.textContent = JSON.stringify(params, null, 2);
-
-    document.getElementById("authForm").submit();
-    axios.post(urlParams("redirect_uri"), params, {
-      headers: { "Content-Type": "x-www-form-urlencoded" },
-      Authorization: urlParams("state"),
-    });
-  });
+    axios
+      .post("https://tst-tsugi.curriki.org/lti/oidc_launch", state, {
+        headers: { "Content-Type": "x-www-form-urlencoded" },
+        Authorization: urlParams("state"),
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
 
   return (
     <Fragment>
